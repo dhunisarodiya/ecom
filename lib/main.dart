726d1404d6b94329bec9ef86c412e4bddbf5ae21
shayari <dhunisarodiya@gmail.com>
2022-06-15 16:56:16 +0530

    nn

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class ecom extends StatefulWidget {
 }
 
 class _ecomState extends State<ecom> {
-  final ImagePicker _picker = ImagePicker();
+  // final ImagePicker _picker = ImagePicker();
   // bool namestatus=false;
   // TextEditingController name=TextEditingController();
   // Map map={
@@ -27,8 +27,6 @@ class _ecomState extends State<ecom> {
   //
   // Come come=Come.fromjson(Map);
 
-
-
   @override
   Widget build(BuildContext context) {
     Map map1={
@@ -37,78 +35,79 @@ class _ecomState extends State<ecom> {
       "score":132,
     };
     Welcome welcome=Welcome.json(map1);
-    print("==${welcome.id}");
-    return Scaffold(
-      body: SafeArea(
-        child: Column(children: [
-          InkWell(onTap: () {
-            showDialog( builder: (context) {
-              return Dialog(
-                child: Column(children: [
-                  ElevatedButton.icon(onPressed: () async {
-                    final XFile? image = await _picker.pickImage(source: ImageSource.gallery);
-                    setState(() {
-                      imag=image!.path;
-                      Navigator.pop(context);
-                      });
-                    },
-                    icon: Icon(Icons.photo), label: Text("gallery",
-                      style: TextStyle(
-                        fontSize: 15,
-                        fontWeight: FontWeight.bold),)),
-                  ElevatedButton.icon(onPressed: () async {
-                    final XFile? photo = await _picker.pickImage(source: ImageSource.camera);
-                    setState(() {
-                      imag=photo!.path;
-                      Navigator.pop(context);
-                     });
-                    },
-                    icon: Icon(Icons.camera), label: Text("camera",
-                      style: TextStyle(
-                        fontSize: 15,
-                        fontWeight: FontWeight.bold),))
-                  ],),
-                );
-              },context: context);
-            },
-            child:imag!="" ?
-            Center(
-              child: Container(
-                child: CircleAvatar(radius: 100,
-                  backgroundImage: FileImage(File(imag)),),),
-              )
-            :Center(
-              child: Container(
-                child: CircleAvatar(radius: 100,
-                  backgroundImage: AssetImage("images/user.png"),),),
-              ),
-           ),
-          Container(margin: EdgeInsets.fromLTRB(35, 10, 35, 10),
-            child: TextField(//controller: name,
-              decoration: InputDecoration(
-                border: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
-                labelText: "Enter Name",
-                hintText: "enter name",
-                //errorText: namestatus?"Enter your valid name" :null
-              ),
-            ),
-          ),
-          // ElevatedButton(onPressed: () {
-          //   namestatus=false;
-          //   String nam=name.text;
-          //   setState(() {
-          //     if(name=="")
-          //       {
-          //         namestatus=true;
-          //       }
-          //   });
-          //
-          // },child: Text("Sign Up"))
-      ],)
-      ),
-    );
+    print("id==${welcome.id}");
+    return Container();
+    // Scaffold(
+    //   body: SafeArea(
+    //       child: Column(children: [
+    //         InkWell(onTap: () {
+    //           showDialog( builder: (context) {
+    //             return Dialog(
+    //               child: Column(children: [
+    //                 ElevatedButton.icon(onPressed: () async {
+    //                   final XFile? image = await _picker.pickImage(source: ImageSource.gallery);
+    //                   setState(() {
+    //                     imag=image!.path;
+    //                     Navigator.pop(context);
+    //                   });
+    //                 },
+    //                     icon: Icon(Icons.photo), label: Text("gallery",
+    //                       style: TextStyle(
+    //                           fontSize: 15,
+    //                           fontWeight: FontWeight.bold),)),
+    //                 ElevatedButton.icon(onPressed: () async {
+    //                   final XFile? photo = await _picker.pickImage(source: ImageSource.camera);
+    //                   setState(() {
+    //                     imag=photo!.path;
+    //                     Navigator.pop(context);
+    //                   });
+    //                 },
+    //                     icon: Icon(Icons.camera), label: Text("camera",
+    //                       style: TextStyle(
+    //                           fontSize: 15,
+    //                           fontWeight: FontWeight.bold),))
+    //               ],),
+    //             );
+    //           },context: context);
+    //         },
+    //           child:imag!="" ?
+    //           Center(
+    //             child: Container(
+    //               child: CircleAvatar(radius: 100,
+    //                 backgroundImage: FileImage(File(imag)),),),
+    //           )
+    //               :Center(
+    //             child: Container(
+    //               child: CircleAvatar(radius: 100,
+    //                 backgroundImage: AssetImage("images/user.png"),),),
+    //           ),
+    //         ),
+    //         Container(margin: EdgeInsets.fromLTRB(35, 10, 35, 10),
+    //           child: TextField(//controller: name,
+    //             decoration: InputDecoration(
+    //               border: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
+    //               labelText: "Enter Name",
+    //               hintText: "enter name",
+    //               //errorText: namestatus?"Enter your valid name" :null
+    //             ),
+    //           ),
+    //         ),
+    //         // ElevatedButton(onPressed: () {
+    //         //   namestatus=false;
+    //         //   String nam=name.text;
+    //         //   setState(() {
+    //         //     if(name=="")
+    //         //       {
+    //         //         namestatus=true;
+    //         //       }
+    //         //   });
+    //         //
+    //         // },child: Text("Sign Up"))
+    //       ],)
+    //   ),
+    // )
   }
-  String imag="";
+  // String imag="";
 
 }
 
