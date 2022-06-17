f73ec07f6495062d626fcb75c0faaac4071d112b
shayari <dhunisarodiya@gmail.com>
2022-06-17 15:26:04 +0530

    nn

--- lib/json.dart
+++ lib/json.dart
@@ -1,3 +1,5 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 
 class json extends StatefulWidget {
@@ -9,10 +11,222 @@ class json extends StatefulWidget {
 
 class _jsonState extends State<json> {
 
+  List<Map> list=[
+    {
+      "albumId":1,
+      "id":1,
+      "title":"BuildContext context",
+      "url":"http://placehold.it/600/92c950",
+      "thumbnailUrl":"http://placehold.it/150/92c95"
+    },
+    {
+      "albumId":1,
+      "id":2,
+      "title":"BuildContext context context",
+      "url":"http://placehold.it/600/771796",
+      "thumbnailUrl":"http://placehold.it/150/771796"
+    },
+    {
+      "albumId":1,
+      "id":3,
+      "title":"BuildContext context BuildContext",
+      "url":"http://placehold.it/600/24f355",
+      "thumbnailUrl":"http://placehold.it/150/24f355"
+    },
+  ];
+  Map map4={
+    "id":1,
+    "name":"productName",
+    "images":[
+      {
+        "id":11,
+        "imageName":"xCh-thy"
+      },
+      {
+        "id":31,
+        "imageName":"fjs-eun"
+      }
+    ]
+  };
+  Map map3={
+    "shape_name":"rectangle",
+    "property":{
+      "width":5.0,
+      "breadth":10.0
+    }
+  };
+  Map map2={
+    "city":"mimbai",
+    "streets":[
+      "address1",
+      "address2"]
+  };
+  Map map1={
+    "id":"6574",
+    "name":"bhvks",
+    "score":132,
+  };
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    Welcome welcome=Welcome.json(map1);
+    print("id==${welcome.id}");
 
+    Welcome1 welcome1=Welcome1.wel(map2);
+    print("streets==${welcome1.streets[1]}");
+
+
+    Welcome3 welcome3 =Welcome3.fromjson(map3);
+    print("property==${welcome3.property.width}");
+
+    Welcome4 welcome4 =Welcome4.fromjson(map4);
+    print("imageName==${welcome4.images[1].imageName}");
+
+    List<Welcom5> temp=[];
+    print("url==${temp[1].url}");
+
+    for(int i=0;i<list.length;i++)
+      {
+        Welcom5 wlc=Welcom5.fromjson(list[i]);
+        temp.add(wlc);
+      }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Container();
   }
 }
+class Welcome {
+  // "id":"6574",
+  // "name":"bhvks",
+  // "score":132
+  String id;
+  String name;
+  int score;
+
+  Welcome(this.id, this.name, this.score);
+  factory Welcome.json(Map map1) {
+    return Welcome(map1['id'], map1['name'], map1['score']);
+  }
+}
+
+class Welcome1 {
+
+  // {
+  // "city":"mimbai",
+  // "streets":["address1",
+  // "address2"]
+  // }
+  String city;
+  List<String> streets;
+  Welcome1(this.city, this.streets); //   "city":"mimbai",
+  factory Welcome1.wel(Map map2) {
+    return Welcome1(map2['city'], map2['streets']);
+  }
+}
+
+class Welcome3 {
+  // {
+  // "shape_name":"rectangle",
+  // "property":{
+  // "width":5.0,
+  // "breadth":10.0
+  // }
+  // }
+  String shape_name;
+  Property property;
+
+  Welcome3(this.shape_name, this.property);
+
+  factory Welcome3.fromjson(Map map3) {
+    return Welcome3(map3['shape_name'], Property.fromjson(map3['property']));
+  }
+}
+
+class Property {
+  // {
+  // "width":5.0,
+  // "breadth":10.0
+  // }
+  double width;
+  double breadth;
+
+  Property(this.width, this.breadth);
+
+  factory Property.fromjson(map3) {
+    return Property(map3['width'], map3['breadth']);
+  }
+}
+
+class Welcome4 {
+  // {
+  // "id":1,
+  // "name":"productName",
+  // "images":[
+  // {
+  // "id":11,
+  // "imageName":"xCh-thy"
+  // },
+  // {
+  // "id":31,
+  // "imageName":"fjs-eun"
+  // }
+  // ]
+  // }
+  int id;
+  String name;
+  List<Myimages> images;
+
+  Welcome4(this.id, this.name, this.images);
+
+  factory Welcome4.fromjson(Map map4) {
+    List<Myimages> temp=[];
+    List list=map4['images'];
+    for(int i=0;i<list.length;i++)
+      {
+        Myimages ww=Myimages.fromjson(list[i]);
+        temp.add(ww);
+      }
+    return Welcome4(map4['id'], map4['name'], temp);
+  }
+}
+
+class Myimages {
+  // {
+  // "id":11,
+  // "imageName":"xCh-thy"
+  // }
+  int id;
+  String imageName;
+
+  Myimages(this.id, this.imageName);
+
+  factory Myimages.fromjson(list) {
+    return Myimages(list['id'], list['imageName']);
+  }
+}
+
+class Welcom5 {
+  // {
+  // "albumId":1,
+  // "id":1,
+  // "title":"BuildContext context",
+  // "url":"http://placehold.it/600/92c950",
+  // "thumbnailUrl":"http://placehold.it/150/92c95"
+  // },
+  int albumId;
+  int id;
+  String title;
+  String url;
+  String thumbnailUrl;
+
+  Welcom5(this.albumId, this.id, this.title, this.url, this.thumbnailUrl);
+
+  factory Welcom5.fromjson(Map list) {
+    return Welcom5(list['albumId'], list['id'], list['title'], list['url'], list['thumbnailUrl']);
+
+  }
+}
+
--- lib/main.dart
+++ lib/main.dart
@@ -18,29 +18,25 @@ class ecom extends StatefulWidget {
 
 class _ecomState extends State<ecom> {
   final ImagePicker _picker = ImagePicker();
-  bool namestatus=false;
+  bool namestatus=true;
   bool numberstatus=false;
   bool passwordstatus=false;
   bool emailstatus=false;
+
   TextEditingController name=TextEditingController();
   TextEditingController number=TextEditingController();
   TextEditingController password=TextEditingController();
   TextEditingController email=TextEditingController();
 
-  Map map1={
-    "id":"6574",
-    "name":"bhvks",
-    "score":132,
-  };
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
 
-  Map map2={
-    "city":"mimbai",
-    "streets":["address1",
-    "address2"]
-  };
+
+  }
   @override
   Widget build(BuildContext context) {
-
     double theight = MediaQuery.of(context).size.height;
     double twidth = MediaQuery.of(context).size.width;
     double statusbar = MediaQuery.of(context).padding.top;
@@ -48,162 +44,139 @@ class _ecomState extends State<ecom> {
     double appbarheight = kToolbarHeight;
 
     double body = theight - statusbar - nagibar - appbarheight;
-    Welcome welcome=Welcome.json(map1);
-    Welcome1 welcome1=Welcome1.wel(map2);
+    return Scaffold(
+      body: SafeArea(
+          child: Column(children: [
+            InkWell(onTap: () {
+              showDialog( builder: (context) {
+                return Dialog(
+                  child: Column(children: [
+                    ElevatedButton.icon(onPressed: () async {
+                      final XFile? image = await _picker.pickImage(source: ImageSource.gallery);
+                      setState(() {
+                        imag=image!.path;
+                        Navigator.pop(context);
+                      });
+                    },
+                        icon: Icon(Icons.photo), label: Text("gallery",
+                          style: TextStyle(
+                              fontSize: 15,
+                              fontWeight: FontWeight.bold),)),
+                    ElevatedButton.icon(onPressed: () async {
+                      final XFile? photo = await _picker.pickImage(source: ImageSource.camera);
+                      setState(() {
+                        imag=photo!.path;
+                        Navigator.pop(context);
+                      });
+                    },
+                        icon: Icon(Icons.camera), label: Text("camera",
+                          style: TextStyle(
+                              fontSize: 15,
+                              fontWeight: FontWeight.bold),))
+                  ],),
+                );
+              },context: context);
+            },
+              child:imag!="" ?
+              Center(
+                child: Container(
+                  child: CircleAvatar(radius: 80,
+                    backgroundImage: FileImage(File(imag)),),),
+              )
+                  :Center(
+                child: Container(
+                  child: CircleAvatar(radius: 80,
+                    backgroundImage: AssetImage("images/user.png"),),),
+              ),
+            ),
+            Container(margin: EdgeInsets.fromLTRB(35, 10, 35, 10),
+              height: body/15,
+              child: TextField(controller: name,
+                onChanged: (value){
+                if(name.text!="")
+                  {
+                    setState(() {
+                      namestatus=true;
+                    });
+                  }
+                },
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(borderRadius: BorderRadius.circular(5)),
+                    labelText: "Enter Name",
+                    hintText: "enter name",
+                    errorText: namestatus? "Enter your valid name":null
+                ),
+              ),
+            ),
+            Container(margin: EdgeInsets.fromLTRB(35, 00, 35, 10),
+              height: body/10,
+              child: TextField(controller: number,
+                maxLength: 10,
 
-    print("id==${welcome.id}");
-    print("streets==${welcome1.streets[1]}");
-    return Container();
-    // Scaffold(
-    //   body: SafeArea(
-    //       child: Column(children: [
-    //         InkWell(onTap: () {
-    //           showDialog( builder: (context) {
-    //             return Dialog(
-    //               child: Column(children: [
-    //                 ElevatedButton.icon(onPressed: () async {
-    //                   final XFile? image = await _picker.pickImage(source: ImageSource.gallery);
-    //                   setState(() {
-    //                     imag=image!.path;
-    //                     Navigator.pop(context);
-    //                   });
-    //                 },
-    //                     icon: Icon(Icons.photo), label: Text("gallery",
-    //                       style: TextStyle(
-    //                           fontSize: 15,
-    //                           fontWeight: FontWeight.bold),)),
-    //                 ElevatedButton.icon(onPressed: () async {
-    //                   final XFile? photo = await _picker.pickImage(source: ImageSource.camera);
-    //                   setState(() {
-    //                     imag=photo!.path;
-    //                     Navigator.pop(context);
-    //                   });
-    //                 },
-    //                     icon: Icon(Icons.camera), label: Text("camera",
-    //                       style: TextStyle(
-    //                           fontSize: 15,
-    //                           fontWeight: FontWeight.bold),))
-    //               ],),
-    //             );
-    //           },context: context);
-    //         },
-    //           child:imag!="" ?
-    //           Center(
-    //             child: Container(
-    //               child: CircleAvatar(radius: 80,
-    //                 backgroundImage: FileImage(File(imag)),),),
-    //           )
-    //               :Center(
-    //             child: Container(
-    //               child: CircleAvatar(radius: 80,
-    //                 backgroundImage: AssetImage("images/user.png"),),),
-    //           ),
-    //         ),
-    //         Container(margin: EdgeInsets.fromLTRB(35, 10, 35, 10),
-    //           height: body/15,
-    //           child: TextField(controller: name,
-    //             decoration: InputDecoration(
-    //                 border: OutlineInputBorder(borderRadius: BorderRadius.circular(5)),
-    //                 labelText: "Enter Name",
-    //                 hintText: "enter name",
-    //                 errorText: namestatus? "Enter your valid name":null
-    //             ),
-    //           ),
-    //         ),
-    //         Container(margin: EdgeInsets.fromLTRB(35, 00, 35, 10),
-    //           height: body/10,
-    //           child: TextField(controller: number,
-    //             maxLength: 10,
-    //             keyboardType: TextInputType.phone,
-    //             decoration: InputDecoration(
-    //                 border: OutlineInputBorder(borderRadius: BorderRadius.circular(5)),
-    //                 labelText: "Enter Number",
-    //                 hintText: "enter number",
-    //                 errorText: numberstatus?"Enter your valid number" :null
-    //             ),
-    //           ),
-    //         ),
-    //         Container(margin: EdgeInsets.fromLTRB(35, 00, 35, 10),
-    //           height: body/15,
-    //           child: TextField(controller: password,
-    //             decoration: InputDecoration(
-    //                 border: OutlineInputBorder(borderRadius: BorderRadius.circular(5)),
-    //                 labelText: "Enter password",
-    //                 hintText: "enter password",
-    //                 errorText: passwordstatus?"Enter your valid password" :null
-    //             ),
-    //           ),
-    //         ),
-    //         Container(margin: EdgeInsets.fromLTRB(35, 00, 35, 10),
-    //           height: body/15,
-    //           child: TextField(controller: email,
-    //             decoration: InputDecoration(
-    //                 border: OutlineInputBorder(borderRadius: BorderRadius.circular(5)),
-    //                 labelText: "Enter Email",
-    //                 hintText: "enter email",
-    //                 errorText: emailstatus?"Enter your valid email" :null
-    //             ),
-    //           ),
-    //         ),
-    //         ElevatedButton(onPressed: () {
-    //           namestatus=false;
-    //           numberstatus=false;
-    //           passwordstatus=false;
-    //           emailstatus=false;
-    //
-    //           setState(() {
-    //             if(name.text=="")
-    //             {
-    //               namestatus=true;
-    //             }
-    //             if(number.text=="")
-    //             {
-    //               numberstatus=true;
-    //             }
-    //             if(password.text=="")
-    //             {
-    //               passwordstatus=true;
-    //             }
-    //             if(email.text.contains("@")==false &&
-    //                 email.text.contains(".com")==false)
-    //             {
-    //               emailstatus=true;
-    //             }
-    //           });
-    //
-    //         },child: Text("Sign Up"))
-    //       ],)
-    //   ),
-    // );
+                keyboardType: TextInputType.phone,
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(borderRadius: BorderRadius.circular(5)),
+                    labelText: "Enter Number",
+                    hintText: "enter number",
+                    errorText: numberstatus?"Enter your valid number" :null
+                ),
+              ),
+            ),
+            Container(margin: EdgeInsets.fromLTRB(35, 00, 35, 10),
+              height: body/15,
+              child: TextField(controller: password,
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(borderRadius: BorderRadius.circular(5)),
+                    labelText: "Enter password",
+                    hintText: "enter password",
+                    errorText: passwordstatus?"Enter your valid password" :null
+                ),
+              ),
+            ),
+            Container(margin: EdgeInsets.fromLTRB(35, 00, 35, 10),
+              height: body/15,
+              child: TextField(controller: email,
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(borderRadius: BorderRadius.circular(5)),
+                    labelText: "Enter Email",
+                    hintText: "enter email",
+                    errorText: emailstatus?"Enter your valid email" :null
+                ),
+              ),
+            ),
+            ElevatedButton(onPressed: () {
+              namestatus=false;
+              numberstatus=false;
+              passwordstatus=false;
+              emailstatus=false;
+
+              setState(() {
+                if(name.text=="")
+                {
+                  namestatus=false;
+                }
+                if(number.text=="")
+                {
+                  numberstatus=true;
+                }
+                if(password.text=="")
+                {
+                  passwordstatus=true;
+                }
+                if(email.text.contains("@")==false &&
+                    email.text.contains(".com")==false)
+                {
+                  emailstatus=true;
+                }
+              });
+
+            },child: Text("Sign Up"))
+          ],)
+      ),
+    );
   }
   String imag="";
 }
 
-class Welcome1 {
-  String city;
-  List<String> streets;
-
-  Welcome1(this.city, this.streets); //   "city":"mimbai",
-//   "streets":["address1",
-//   "address2"]
-// };
-
-  factory Welcome1.wel(Map map2) {
-    return Welcome1(map2['city'], map2['streets']);
-  }
-}
-class Welcome {
-  // "id":"6574",
-  // "name":"bhvks",
-  // "score":132
-  String id;
-  String name;
-  int score;
-
-  Welcome(this.id, this.name, this.score);
-  factory Welcome.json(Map map1) {
-    return Welcome(map1['id'], map1['name'], map1['score']);
-  }
-}
 
 
